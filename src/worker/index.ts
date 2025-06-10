903590827357cbef991523982b7a8239e6478fb7
Farel <alexanderkimf@gmail.com>
2025-06-10 21:10:58 +0000

    test

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -2,5 +2,5 @@ import { Hono } from "hono";
 const app = new Hono<{ Bindings: Env }>();
 
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
-
+app.get("/api/ucup", (c) => c.json({ name: "ucup" }));
 export default app;
